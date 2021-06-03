da71013abef266fe098ae5b7dc544ce48063e022
Tong <915712823@qq.com>
2021-06-03 17:31:38 +0800

    fixbug

--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// element-ui
 import './plugins/elementUI'
 
+// 懒加载
 import VueLazyLoad from 'vue-lazyload'
 Vue.use(VueLazyLoad)
 
+// 全局组件库，不需要重复引用
+import './components/globalComponent';
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -8,13 +8,17 @@ VueRouter.prototype.push = function push(location) {
 }
 
 
-const Home = () => import( /* webpackChunkName: "home" */ '@/views/home/Home')
+const Home = () => import( /* webpackChunkName: "home" */ 'views/home/Home')
 
 Vue.use(VueRouter)
 
 const routes = [{
     path: '/',
-    redirect: '/home'
+    redirect: '/home',
+    meta: {
+      requireAuth: false,
+      keepAlive: false
+    },
   },
   {
     path: '/home',
@@ -22,28 +26,14 @@ const routes = [{
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: Home
+    component: Home,
+    meta: {
+      requireAuth: false,
+      keepAlive: false
+    },
   }
 ]
-router.beforeEach((to, from, next) => {
-  const name = sessionStorage.getItem("username");
-  const token = sessionStorage.getItem("token");
 
-  if (to.name === 'Login') {
-    next();
-  } else {
-    if (token) {
-      if (name) {
-        next();
-      } else {
-        router.push("/login")
-      }
-    } else {
-      this.$message.warning('请先登录');
-      router.push("/login");
-    }
-  }
-})
 
 
 const router = new VueRouter({
@@ -52,4 +42,48 @@ const router = new VueRouter({
   routes
 })
 
+
+router.beforeEach((to, from, next) => {
+  const name = sessionStorage.getItem("username");
+  const token = sessionStorage.getItem("token");
+
+  if (to.matched.some(record => record.meta.requireAuth)) {
+    if (to.name === 'Login') {
+      next()
+    } else {
+      if (token) {
+        //访问服务器缓存数据，判断当前token是否失效
+        // Vue.http.get("http:xxxx/Login/UserIsLogin?token=" + localStorage.getItem('token') + "&url=" + to.name, {
+        //   withCredentials: true
+        // }).then(response => response.json()).then(num => {
+        //   // console.log('是否登录',num);
+        //   if (num.code == 1001) {
+        //     next();
+        //   } else {
+        //     alert('您的token已超时，请重新登录');
+        //     next('/Login');
+        //   }
+        // })
+      } else {
+        next('/login')
+      }
+    }
+  } else {
+    next()
+  }
+  // if (to.name === 'Login') {
+  //   next();
+  // } else {
+  //   if (token) {
+  //     if (name) {
+  //       next();
+  //     } else {
+  //       router.push("/login")
+  //     }
+  //   } else {
+  //     this.$message.warning('请先登录');
+  //     router.push("/login");
+  //   }
+  // }
+})
 export default router
